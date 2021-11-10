c78eb9249c49239514a64708bceba2674ff364ec
Christian Müller <christian@dfinity.org>
2021-11-10 10:29:40 +0100

    refactoring

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var URL = "https://ic-api.internetcomputer.org/api/v3/proposals?limit=1"
-var statePath = "state.json"
-var checkInterval = 5 * time.Minute
+var (
+	URL                        = "https://ic-api.internetcomputer.org/api/v3/proposals?limit=1"
+	STATE_PATH                 = "state.json"
+	NNS_POLL_INTERVALL         = 5 * time.Minute
+	STATE_PERSISTENCE_INTERVAL = time.Minute
+	MAX_TOPIC_LENGTH           = 30
+	MAX_BLOCKED_TOPICS         = 30
+)
 
 type Proposal struct {
 	Title   string `json:"title"`
@@ -33,30 +38,104 @@ type State struct {
 }
 
 func (s *State) persist() {
+	s.lock.RLock()
 	data, err := json.Marshal(s)
+	s.lock.RUnlock()
 	if err != nil {
 		log.Println("Couldn't serialize state:", err)
 		return
 	}
-	err = os.WriteFile(statePath, data, 0644)
+	err = os.WriteFile(STATE_PATH, data, 0644)
 	if err != nil {
-		log.Println("Couldn't write to state file", statePath, " :", err)
+		log.Println("Couldn't write to state file", STATE_PATH, " :", err)
 	}
-	log.Println(len(data), "bytes persisted to", statePath)
+	log.Println(len(data), "bytes persisted to", STATE_PATH)
 }
 
 func (s *State) restore() {
-	data, err := os.ReadFile(statePath)
+	data, err := os.ReadFile(STATE_PATH)
 	if err != nil {
-		log.Println("Couldn't read file", statePath)
+		log.Println("Couldn't read file", STATE_PATH)
 	}
 	if err := json.Unmarshal(data, &s); err != nil {
-		log.Println("Couldn't deserialize the state file", statePath, ":", err)
+		log.Println("Couldn't deserialize the state file", STATE_PATH, ":", err)
 	}
 	if s.ChatIds == nil {
 		s.ChatIds = map[int64]map[string]bool{}
 	}
 	fmt.Println("Deserialized the state with", len(s.ChatIds), "users, last proposal id:", s.LastSeenProposal)
+}
+
+func (s *State) setNewLastSeenId(id int64) (updated bool) {
+	s.lock.Lock()
+	if s.LastSeenProposal != id {
+		s.LastSeenProposal = id
+		updated = true
+	}
+	s.lock.Unlock()
+	return
+}
+
+func (s *State) removeChatId(id int64) {
+	s.lock.Lock()
+	delete(s.ChatIds, id)
+	s.lock.Unlock()
+	log.Println("Removed user", id, "from subscribers")
+}
+
+func (s *State) addChatId(id int64) {
+	s.lock.Lock()
+	s.ChatIds[id] = map[string]bool{}
+	s.lock.Unlock()
+	log.Println("Added user", id, "to subscribers")
+}
+
+func (s *State) blockTopic(id int64, topic string) {
+	if len(topic) > MAX_TOPIC_LENGTH {
+		return
+	}
+	s.lock.Lock()
+	blacklist := s.ChatIds[id]
+	if blacklist != nil && len(blacklist) < MAX_BLOCKED_TOPICS {
+		blacklist[topic] = true
+	}
+	s.lock.Unlock()
+}
+
+func (s *State) unblockTopic(id int64, topic string) {
+	s.lock.Lock()
+	blacklist := s.ChatIds[id]
+	if blacklist != nil {
+		delete(blacklist, topic)
+	}
+	s.lock.Unlock()
+}
+
+func (s *State) chatIds(topic string) (res []int64) {
+	s.lock.RLock()
+	for id, blacklist := range s.ChatIds {
+		if blacklist != nil && !blacklist[topic] {
+			res = append(res, id)
+		}
+	}
+	s.lock.RUnlock()
+	return
+}
+
+func (s *State) blockedTopics(id int64) string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	m := s.ChatIds[id]
+	if m == nil || len(m) == 0 {
+		return "You did not block any topics."
+	}
+	var res []string
+	for topic, enabled := range m {
+		if enabled {
+			res = append(res, topic)
+		}
+	}
+	return fmt.Sprintf("You've blocked these topics: %s", strings.Join(res, ", "))
 }
 
 func main() {
@@ -73,62 +152,47 @@ func main() {
 	state.restore()
 
 	go fetchProposalsAndNotify(bot, &state)
+	go persist(&state)
 
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
-		var msg tgbotapi.MessageConfig
+		var msg string
 		id := update.Message.Chat.ID
-		userMsg := strings.ToLower(update.Message.Text)
-		subscription := userMsg == "/start"
-		block := strings.Contains(userMsg, "/block")
-		if subscription || userMsg == "/stop" {
-			var text string
-			state.lock.Lock()
-			if subscription {
-				state.ChatIds[id] = map[string]bool{}
-				log.Println("Added user", id, "to subscribers")
-				text = "Subscribed." + "\n\n" + getHelpMessage()
-			} else {
-				delete(state.ChatIds, id)
-				log.Println("Removed user", id, "from subscribers")
-				text = "Unsubscribed."
-			}
-			state.persist()
-			state.lock.Unlock()
-			msg = tgbotapi.NewMessage(id, text)
-		} else if block || strings.Contains(userMsg, "/unblock") {
-			words := strings.Split(userMsg, " ")
-			var text string
-			if len(words) != 2 {
-				text = fmt.Sprintf("Please specify the topic")
-			} else {
-				state.lock.Lock()
-				topic := strings.Replace(words[1], "#", "", -1)
-				if block {
-					blacklist := state.ChatIds[id]
-					if blacklist != nil {
-						blacklist[topic] = true
-					}
-				} else {
-					delete(state.ChatIds[id], topic)
-				}
-				text = blockedTopicsMsg(state.ChatIds[id])
-				state.persist()
-				state.lock.Unlock()
-			}
-			msg = tgbotapi.NewMessage(id, text)
-		} else if userMsg == "/blacklist" {
-			state.lock.RLock()
-			text := blockedTopicsMsg(state.ChatIds[id])
-			state.lock.RUnlock()
-			msg = tgbotapi.NewMessage(id, text)
-		} else {
-			msg = tgbotapi.NewMessage(id, getHelpMessage())
+		words := strings.Split(strings.ToLower(update.Message.Text), " ")
+		if len(words) == 0 {
+			continue
 		}
-		bot.Send(msg)
+		cmd := words[0]
+		switch cmd {
+		case "/start", "/stop":
+			if cmd == "/start" {
+				state.addChatId(id)
+				msg = "Subscribed." + "\n\n" + getHelpMessage()
+			} else {
+				state.removeChatId(id)
+				msg = "Unsubscribed."
+			}
+		case "/block", "/unblock":
+			if len(words) != 2 {
+				msg = fmt.Sprintf("Please specify the topic")
+			} else {
+				topic := strings.Replace(words[1], "#", "", -1)
+				if cmd == "/block" {
+					state.blockTopic(id, topic)
+				} else {
+					state.unblockTopic(id, topic)
+				}
+				msg = state.blockedTopics(id)
+			}
+		case "/blacklist":
+			msg = state.blockedTopics(id)
+		default:
+			msg = getHelpMessage()
+		}
+		bot.Send(tgbotapi.NewMessage(id, msg))
 	}
 }
 
@@ -138,8 +202,15 @@ func getHelpMessage() string {
 		"use /blacklist to display the list of blocked topics."
 }
 
+func persist(state *State) {
+	ticker := time.NewTicker(STATE_PERSISTENCE_INTERVAL)
+	for range ticker.C {
+		state.persist()
+	}
+}
+
 func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, state *State) {
-	ticker := time.NewTicker(checkInterval)
+	ticker := time.NewTicker(NNS_POLL_INTERVALL)
 	for range ticker.C {
 		resp, err := http.Get(URL)
 		if err != nil {
@@ -154,57 +225,28 @@ func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, state *State) {
 		}
 		if err := json.Unmarshal(body, &jsonResp); err != nil {
 			fmt.Println("Couldn't parse the response as JSON:", err)
-		} else {
-			proposal := jsonResp.Data[0]
-			var lastSeenProposal int64
-			state.lock.RLock()
-			lastSeenProposal = state.LastSeenProposal
-			state.lock.RUnlock()
-
-			if lastSeenProposal == proposal.Id {
-				continue
-			}
-			log.Println("New proposal detected:", proposal)
-			summary := proposal.Summary
-			if len(summary) > 0 {
-				summary = "\n" + summary + "\n"
-			}
-			text := fmt.Sprintf("*%s*\n%s\n#%s\n\nhttps://dashboard.internetcomputer.org/proposal/%d",
-				proposal.Title, summary, tgbotapi.EscapeText(tgbotapi.ModeMarkdown, proposal.Topic), proposal.Id)
-
-			state.lock.Lock()
-			state.LastSeenProposal = proposal.Id
-			state.persist()
-			state.lock.Unlock()
-
-			state.lock.RLock()
-			msgs := 0
-		USERS:
-			for id, blacklist := range state.ChatIds {
-				if blacklist[strings.ToLower(proposal.Topic)] {
-					continue USERS
-				}
-				msg := tgbotapi.NewMessage(id, text)
-				msg.ParseMode = tgbotapi.ModeMarkdown
-				msg.DisableWebPagePreview = true
-				bot.Send(msg)
-				msgs += 1
-			}
-			log.Println("Successfully notified", msgs, "users")
-			state.lock.RUnlock()
+			continue
 		}
-	}
-}
 
-func blockedTopicsMsg(m map[string]bool) string {
-	if len(m) == 0 {
-		return "You did not block any topics."
-	}
-	var res []string
-	for topic, enabled := range m {
-		if enabled {
-			res = append(res, topic)
+		proposal := jsonResp.Data[0]
+		if !state.setNewLastSeenId(proposal.Id) {
+			continue
 		}
+		log.Println("New proposal detected:", proposal)
+		summary := proposal.Summary
+		if len(summary) > 0 {
+			summary = "\n" + summary + "\n"
+		}
+		text := fmt.Sprintf("*%s*\n%s\n#%s\n\nhttps://dashboard.internetcomputer.org/proposal/%d",
+			proposal.Title, summary, tgbotapi.EscapeText(tgbotapi.ModeMarkdown, proposal.Topic), proposal.Id)
+
+		ids := state.chatIds(strings.ToLower(proposal.Topic))
+		for _, id := range ids {
+			msg := tgbotapi.NewMessage(id, text)
+			msg.ParseMode = tgbotapi.ModeMarkdown
+			msg.DisableWebPagePreview = true
+			bot.Send(msg)
+		}
+		log.Println("Successfully notified", len(ids), "users")
 	}
-	return fmt.Sprintf("You've blocked these topics: %s", strings.Join(res, ", "))
 }
