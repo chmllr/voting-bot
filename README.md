14f67d548d34b836b256db24c9076af3bcd31514
Christian Müller <christian@dfinity.org>
2022-02-18 15:57:55 +0100

    stripping down

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
-NNS Proposals Bot
-=================
-
-A [telegram bot](https://t.me/NNSProposalsBot) alerting about all new NNS proposals.
-Supports filtering on the proposal topic.
-
-## Compilation & Execution
-
-Compile the bot using `go build`; this will create an executable `nns-proposals-bot`.
-Run the bot with the authentication token provided in the `TOKEN` environment variable:
-
-    TOKEN=<...> ./nns-proposals-bot
-
-## Interaction with the bot
-
-Enter `/start` to subscribe to the notifications and use `/stop` to cancel the subscription.
-Use `/block` or `/unblock` to block or unblock proposals with a certain topic.
-Use `/blacklist` to display the list of blocked topics.
-Use `/governance_only` to block all topics except governance.
+Voting Bot
+==========
--- main.go
+++ main.go
@@ -8,23 +8,18 @@ import (
 	"net/http"
 	"os"
 	"sort"
-	"strings"
-	"sync"
+	"strconv"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var (
-	URL                        = "https://ic-api.internetcomputer.org/api/v3/proposals?limit=100"
-	STATE_PATH                 = "state.json"
-	NNS_POLL_INTERVALL         = 5 * time.Minute
-	STATE_PERSISTENCE_INTERVAL = 5 * time.Minute
-	MAX_TOPIC_LENGTH           = 50
-	MAX_BLOCKED_TOPICS         = 30
-	MAX_SUMMARY_LENGTH         = 2048
-	TOPIC_GOVERNANCE           = "topic_governance"
-	ALL_EXCEPT_GOVERNANCE      = "all_except_governance"
+	URL                = "https://ic-api.internetcomputer.org/api/v3/proposals?limit=100"
+	NNS_POLL_INTERVALL = 5 * time.Minute
+	MAX_SUMMARY_LENGTH = 2048
+	TOPIC_GOVERNANCE   = "topic_governance"
+	LAST_SEEN_PROPOSAL = int64(0)
 )
 
 type Proposal struct {
@@ -36,211 +31,40 @@ type Proposal struct {
 	Proposer string `json:"proposer"`
 }
 
-type State struct {
-	LastSeenProposal int64                     `json:"last_seen_proposal"`
-	ChatIds          map[int64]map[string]bool `json:"chat_ids"`
-	lock             sync.RWMutex
-}
-
-// Locks the state, persists it to a temporary file, then moves the temporary
-// file to the location of the persisted state. This should avoid broken state
-// if the process gets killed in the middle of writing.
-func (s *State) persist() {
-	s.lock.RLock()
-	data, err := json.Marshal(s)
-	s.lock.RUnlock()
-	if err != nil {
-		log.Println("Couldn't serialize state:", err)
-		return
-	}
-	tmpFile, err := ioutil.TempFile(".", STATE_PATH+"_tmp_")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = os.WriteFile(tmpFile.Name(), data, 0644)
-	if err != nil {
-		log.Println("Couldn't write to state file", STATE_PATH, " :", err)
-	}
-	os.Rename(tmpFile.Name(), STATE_PATH)
-	log.Println(len(data), "bytes persisted to", STATE_PATH)
-}
-
-// Deserialize the persisted state from the disk. Currently, prints an error on a first run.
-func (s *State) restore() {
-	data, err := os.ReadFile(STATE_PATH)
-	if err != nil {
-		log.Println("Couldn't read file", STATE_PATH)
-	}
-	if err := json.Unmarshal(data, &s); err != nil {
-		log.Println("Couldn't deserialize the state file", STATE_PATH, ":", err)
-	}
-	if s.ChatIds == nil {
-		s.ChatIds = map[int64]map[string]bool{}
-	}
-	fmt.Println("Deserialized the state with", len(s.ChatIds), "users, last proposal id:", s.LastSeenProposal)
-}
-
-// This is an atomic compare and swap for a new seen proposal id.
-func (s *State) setNewLastSeenId(id int64) (updated bool) {
-	s.lock.Lock()
-	if s.LastSeenProposal < id {
-		s.LastSeenProposal = id
-		updated = true
-	}
-	s.lock.Unlock()
-	return
-}
-
-// Unsubscribes the chat id.
-func (s *State) removeChatId(id int64) {
-	s.lock.Lock()
-	delete(s.ChatIds, id)
-	s.lock.Unlock()
-	log.Println("Removed user", id, "from subscribers")
-}
-
-// Subscribes the chat id.
-func (s *State) addChatId(id int64) {
-	s.lock.Lock()
-	s.ChatIds[id] = map[string]bool{"topic_exchange_rate": true}
-	s.lock.Unlock()
-	log.Println("Added user", id, "to subscribers")
-}
-
-// Block `topic` for chat `id`. Checks max topic length and max blocked topics to avoid
-// trivial bloat attacks.
-func (s *State) blockTopic(id int64, topic string) {
-	if len(topic) > MAX_TOPIC_LENGTH {
-		return
-	}
-	s.lock.Lock()
-	blacklist := s.ChatIds[id]
-	if blacklist != nil && len(blacklist) < MAX_BLOCKED_TOPICS {
-		blacklist[topic] = true
-	}
-	s.lock.Unlock()
-}
-
-// Unblocks `topic` for chat `id`.
-func (s *State) unblockTopic(id int64, topic string) {
-	s.lock.Lock()
-	blacklist := s.ChatIds[id]
-	if blacklist != nil {
-		delete(blacklist, topic)
-	}
-	s.lock.Unlock()
-}
-
-// Returns the list of chat ids which should be notified about `topic`.
-func (s *State) chatIdsForTopic(topic string) (res []int64) {
-	s.lock.RLock()
-	for id, blacklist := range s.ChatIds {
-		// Skip if no blacklist or topic is blacklisted.
-		if blacklist == nil || blacklist[topic] {
-			continue
-		}
-		// Skip if only governance topic is whitelisted and the topic is not governance.
-		if blacklist[ALL_EXCEPT_GOVERNANCE] && topic != TOPIC_GOVERNANCE {
-			continue
-		}
-		res = append(res, id)
-	}
-	s.lock.RUnlock()
-	return
-}
-
-// Returns a string of blocked topics.
-func (s *State) blockedTopics(id int64) string {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
-	m := s.ChatIds[id]
-	if m == nil || len(m) == 0 {
-		return "Your list of blocked topics is empty."
-	}
-	var res []string
-	for topic, enabled := range m {
-		if enabled {
-			res = append(res, topic)
-		}
-	}
-	return fmt.Sprintf("You've blocked these topics: %s.", strings.Join(res, ", "))
-}
-
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
 	if err != nil {
 		log.Panic("Couldn't instantiate the bot API:", err)
 	}
 
+	chatId, err := strconv.ParseInt(os.Getenv("CHAT"), 10, 64)
+	if err != nil {
+		log.Panic("Couldn't parse the chat id:", err)
+	}
+
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	var state State
-	state.restore()
-
-	go fetchProposalsAndNotify(bot, &state)
-	go persist(&state)
+	go fetchProposalsAndNotify(bot, chatId)
 
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-		var msg string
 		id := update.Message.Chat.ID
-		words := strings.Split(strings.ToLower(update.Message.Text), " ")
-		if len(words) == 0 {
+		if update.Message == nil || id != chatId {
 			continue
 		}
-		cmd := words[0]
-		switch cmd {
-		case "/start":
-			state.addChatId(id)
-			msg = "Subscribed." + "\n\n" + getHelpMessage()
-		case "/stop":
-			state.removeChatId(id)
-			msg = "Unsubscribed."
-		case "/block", "/unblock":
-			if len(words) != 2 {
-				msg = fmt.Sprintf("Please specify the topic")
-				break
-			}
-			topic := strings.Replace(words[1], "#", "", -1)
-			switch cmd {
-			case "/block":
-				state.blockTopic(id, topic)
-			default:
-				state.unblockTopic(id, topic)
-			}
-			msg = state.blockedTopics(id)
-		case "/governance_only":
-			state.blockTopic(id, ALL_EXCEPT_GOVERNANCE)
-			msg = "From now on, you'll only see the governance proposals."
-		case "/blacklist":
-			msg = state.blockedTopics(id)
-		default:
-			msg = getHelpMessage()
+		if update.Message.Text == "/YES" {
+			fmt.Println("voted yes")
+		} else {
+			fmt.Println("voted no")
 		}
+		msg := "Voted!"
 		bot.Send(tgbotapi.NewMessage(id, msg))
 	}
 }
 
-func getHelpMessage() string {
-	return "Enter /stop to unsubscribe (/start to resubscribe). " +
-		"Use /block or /unblock to block or unblock proposals with a certain a topic; " +
-		"use /blacklist to display the list of blocked topics. " +
-		"Use /governance_only command to only receive governance proposals."
-}
-
-func persist(state *State) {
-	ticker := time.NewTicker(STATE_PERSISTENCE_INTERVAL)
-	for range ticker.C {
-		state.persist()
-	}
-}
-
-func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, state *State) {
+func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, id int64) {
 	ticker := time.NewTicker(NNS_POLL_INTERVALL)
 	for range ticker.C {
 		resp, err := http.Get(URL)
@@ -263,10 +87,11 @@ func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, state *State) {
 		sort.Slice(proposals, func(i, j int) bool { return proposals[i].Id < proposals[j].Id })
 
 		for _, proposal := range jsonResp.Data {
-			if !state.setNewLastSeenId(proposal.Id) {
+			if proposal.Id == LAST_SEEN_PROPOSAL { // || proposal.Topic != TOPIC_GOVERNANCE {
 				continue
 			}
-			log.Println("New proposal detected:", proposal)
+			LAST_SEEN_PROPOSAL = proposal.Id
+			log.Println("New governance proposal detected:", proposal)
 			summary := proposal.Summary
 			if len(summary)+2 > MAX_SUMMARY_LENGTH {
 				summary = "[Proposal summary is too long.]"
@@ -276,22 +101,14 @@ func fetchProposalsAndNotify(bot *tgbotapi.BotAPI, state *State) {
 			}
 			text := fmt.Sprintf("<b>%s</b>\n\nProposer: %s\n%s\n#%s\n\nhttps://dashboard.internetcomputer.org/proposal/%d",
 				proposal.Title, proposal.Proposer, summary, proposal.Topic, proposal.Id)
+			text = text + "\n\n/YES || /NO"
 
-			ids := state.chatIdsForTopic(strings.ToLower(proposal.Topic))
-			for _, id := range ids {
-				msg := tgbotapi.NewMessage(id, text)
-				msg.ParseMode = tgbotapi.ModeHTML
-				msg.DisableWebPagePreview = true
-				_, err := bot.Send(msg)
-				if err != nil {
-					log.Println("Couldn't send message:", err)
-					if strings.Contains(err.Error(), "bot was blocked by the user") {
-						state.removeChatId(id)
-					}
-				}
-			}
-			if len(ids) > 0 {
-				log.Println("Successfully notified", len(ids), "users")
+			msg := tgbotapi.NewMessage(id, text)
+			msg.ParseMode = tgbotapi.ModeHTML
+			msg.DisableWebPagePreview = true
+			_, err := bot.Send(msg)
+			if err != nil {
+				log.Println("Couldn't send message:", err)
 			}
 		}
 	}
